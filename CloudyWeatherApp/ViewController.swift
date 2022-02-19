1b929a0f829fee04e05d36de070510172c2a6a41
Carleton Snow <chip90@gmail.com>
2022-02-19 08:27:59 -0500

    Initial Commit

---
+++ CloudyWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudyWeatherApp
+//
+//  Created by Carleton C Snow III on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
